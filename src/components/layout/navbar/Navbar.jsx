3a37ad9627c36e07ed4cada84cb9a7414d29d7a4
Abdulkadir Adamu <adamuhammadabdulkadir@gmail.com>
2022-05-29 22:29:06 +0100

    navbar toggle

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
+import useOnclickOutside from 'react-cool-onclickoutside';
+import { FaAngleDown } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import '../navbar/navbar.css';
 import { menuData } from '../../../data/navbar.data';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggling = () => setIsOpen(!isOpen);
+
+  const ref = useOnclickOutside(() => {
+    setIsOpen(false);
+  });
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <h1>Starking</h1>
+          <h1>
+            <Link to="/">Starking</Link>
+          </h1>
         </div>
 
         <nav>
@@ -15,14 +27,20 @@ function Navbar() {
             {menuData.map((data, index) =>
               data.submenu ? (
                 <li className="submenu">
-                  <button>{data.title}</button>
-                  <ul className="submenu-items">
-                    {data.submenu.map((submenu) => (
-                      <li>
-                        <Link to={submenu.title}>{submenu.title}</Link>
-                      </li>
-                    ))}
-                  </ul>
+                  <button onClick={toggling}>
+                    {data.title} <FaAngleDown />
+                  </button>
+                  {isOpen && (
+                    <ul className="submenu-items" ref={ref}>
+                      {data.submenu.map((submenu) => (
+                        <li>
+                          <Link to={submenu.title} onClick={toggling}>
+                            {submenu.title}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
                 </li>
               ) : (
                 <li key={index}>
